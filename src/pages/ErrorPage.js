a9c9d0dc936e9f5e602696c9d198626f83ac7cde
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-05 20:05:14 +0200

    reworked error page a bit. added links

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import home from "../assets/home.svg";
+import forum from "../assets/forum.svg";
 
 export const ErrorPage = () => {
   return (
     <div className="errorPage">
       <h1 className="title">Error page</h1>
-      <Link to="/">
-        <button className="btn">Back home</button>
+      <div>
+        <p style={{ margin: "1rem" }}>Back to...</p>
+      </div>
+      <Link to="/" style={{ margin: "1rem", fontSize: "1.25rem" }}>
+        Home Page
+      </Link>
+      <Link to="/posts" style={{ margin: "1rem", fontSize: "1.25rem" }}>
+        Posts
       </Link>
     </div>
   );
