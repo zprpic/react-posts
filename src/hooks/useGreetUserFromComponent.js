a8fbcf4f6f546a3fe86e3c5418e5c86b6b55916f
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-13 08:48:33 +0200

    Cleaned up custom greetFromComponent hook

--- src/hooks/useGreetUserFromComponent.js
+++ src/hooks/useGreetUserFromComponent.js
@@ -2,8 +2,7 @@ import { useRef } from "react";
 import { useEffect } from "react";
 
 export const useGreetUserFromComponent = (message, name) => {
-  const refContainer = useRef(name);
-  const componentName = refContainer.current.name;
+  const componentName = useRef(name).current.name;
 
   useEffect(() => {
     console.log(`${message} ${componentName}`);
